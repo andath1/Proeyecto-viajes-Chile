9908378aec6c457eca79142f1da3a677bfefef77
andres rojas <andath1@hotmail.com>
2022-01-03 09:03:15 -0300

    se agregan todas los requisitos al proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 $(function() {
+    /* se agrega efecto de esconder texto en seccion destacados, trajetas */
     $(".card-title").click(function() {
         $(".card-text").toggle("slow", function() {
 
         });
     });
-
+    /* se agrega efecto tooltip en etiqueta navbar, palabra "inicio" */
     $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     })
